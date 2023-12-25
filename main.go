635dc86308b3b42ec36d261bda4b9056525463bf
Abhinav Sinha <abhinavvsinhaa@gmail.com>
2023-12-25 09:23:37 +0530

    Added chi router and cors dependencies, set up server

--- main.go
+++ main.go
@@ -3,18 +3,35 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 
+	"github.com/go-chi/chi"
 	"github.com/joho/godotenv"
 )
 
 func main() {
-	godotenv.Load(".env")
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Could not load environment")
+	}
 
 	port := os.Getenv("PORT")
 	if port == "" {
 		log.Fatal("PORT not found in environment")
 	}
 
-	fmt.Println("port: ", port)
+	router := chi.NewRouter()
+
+	srv := &http.Server{
+		Handler: router,
+		Addr:    ":" + port,
+	}
+
+	fmt.Printf("Server starting on port %v", port)
+
+	err = srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
