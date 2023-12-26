484b49528d8ce0531478225d060cb871f1d85d86
Abhinav Sinha <abhinavvsinhaa@gmail.com>
2023-12-26 20:53:27 +0530

    SQLC: Feed follows Queries

--- main.go
+++ main.go
@@ -54,8 +54,12 @@ func main() {
 	v1Router := chi.NewRouter()
 	v1Router.Get("/healthz", handlerReadiness)
 	v1Router.Get("/err", handlerError)
+
 	v1Router.Post("/users", apiCfg.handleCreateUser)
 	v1Router.Get("/users", apiCfg.authMiddleware(apiCfg.handleGetUser))
+
+	v1Router.Post("/feeds", apiCfg.authMiddleware(apiCfg.handleCreateFeed))
+	v1Router.Get("/feeds", apiCfg.handleGetFeeds)
 
 	router.Mount("/v1", v1Router)
 
